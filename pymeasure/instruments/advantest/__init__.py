9eed2a9ba690e1bd464821a9f812e73cd10c40bd
neuschs <sebastian.neusch@homenet-seb.de>
2025-02-13 23:03:00 +0100

    PR feedback

--- pymeasure/instruments/advantest/__init__.py
+++ pymeasure/instruments/advantest/__init__.py
@@ -25,3 +25,4 @@
 from .advantestR3767CG import AdvantestR3767CG
 from .advantestR624X import AdvantestR6245
 from .advantestR624X import AdvantestR6246
+from .advantestR3265A import AdvantestR3265A
